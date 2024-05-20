d07cba6f5386fad1d25e90285825bf346b73a40b
ryanhanks-bestow <ryan.hanks@bestow.world>
2024-05-19 21:53:26 -0500

    chore: initial commit

---
+++ lib/src/flakka_journal.dart
@@ -0,0 +1,7 @@
+/// {@template flakka_journal}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class FlakkaJournal {
+  /// {@macro flakka_journal}
+  const FlakkaJournal();
+}
---
+++ test/src/flakka_journal_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flakka_journal/flakka_journal.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('FlakkaJournal', () {
+    test('can be instantiated', () {
+      expect(FlakkaJournal(), isNotNull);
+    });
+  });
+}
